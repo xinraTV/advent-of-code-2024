b37aeec79c2c2148c6eea626f991ac76069724e2
xinraTV <git@xinra.de>
2024-12-01 13:58:06 +0100

    Day 1

---
+++ Day01/Program.cs
@@ -0,0 +1,30 @@
+﻿
+var contents = File.ReadLines(@"..\..\..\input.txt");
+
+var left = new List<int>();
+var right = new List<int>();
+
+foreach (var line in contents)
+{
+    var parts = line.Split("   ");
+    left.Add(Convert.ToInt32(parts[0]));
+    right.Add(Convert.ToInt32(parts[1]));
+}
+
+left.Sort();
+right.Sort();
+
+var sum = 0;
+
+for (var i = 0; i < left.Count; i++)
+{
+    sum += Math.Abs(left[i] - right[i]);
+}
+
+Console.WriteLine(sum);
+
+// Part 2
+
+var sum2 = left.Sum(l => right.FindAll(r => r == l).Count * l);
+
+Console.WriteLine(sum2);
